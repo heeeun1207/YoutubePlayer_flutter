08ca50073ca979e542ce8acd51768accded599fd
jungheeeun <jungheeeun272@gmai.com>
2023-12-22 18:36:57 +0900

    으으으음..? api

---
+++ YoutubePlayer_APP/lib/const/api.dart
@@ -0,0 +1,6 @@
+const API_KEY ='토큰 입력';
+// Youtube Data API V3 URL
+const YOUTUBE_API_BASE_URL = 'http://youtube.googleapis.com/youtube/v3/search';
+const CF_CHANNEL_ID = 'UCxZ2AIaT0h0mxzZVbF_j_Sw'; // 지정 채널 ID
+
+
