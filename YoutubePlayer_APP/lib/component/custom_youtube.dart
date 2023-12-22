7fe3fe99ac41ef57a7823150da1a4051758d3d23
jungheeeun <jungheeeun272@gmai.com>
2023-12-22 17:30:40 +0900

    유튜브 동영상 재생 관련 위젯 구현

--- YoutubePlayer_APP/lib/component/custom_youtube.dart
+++ YoutubePlayer_APP/lib/component/custom_youtube.dart
@@ -19,8 +19,52 @@ class CustomYotubePlayer extends StatefulWidget {
 }
 
 class _CustomYoutubePlayerState extends State<CustomYotubePlayer> {
+ YoutubePlayerController? controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = YoutubePlayerController( // 컨트롤러 선언
+        initialVideoId: widget.videoModel.id, // 처음 실행할 동영상의 ID
+        flags: YoutubePlayerFlags(
+          autoPlay: false, // 자동 실행 사용하지 않기
+        ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(); // 임시로 기본 컨테이너 반환
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        YoutubePlayer( // 유튜브 재생기 렌더링
+          controller: controller!,
+          showVideoProgressIndicator: true,
+        ),
+        const SizedBox(height: 16.0),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Text(
+            widget.videoModel.title, // 동영상 제목
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ),
+        const SizedBox(height: 16.0),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    controller!.dispose(); // State 폐기 시 컨드롤러 같이 폐기
   }
 }
+
+
